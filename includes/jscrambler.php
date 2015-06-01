dd77161b1450ee0d75b3446510a28e30bf864320
Filipe Silva <filipe.silva@jscrambler.com>
2015-06-01 20:52:36 +0100

    v4 client

--- includes/jscrambler.php
+++ includes/jscrambler.php
@@ -24,18 +24,16 @@ class Jscrambler {
     private $api_port = 80;
     private $api_version = null;
 
-    public function __construct($access_key, $secret_key, $api_host = null, $api_port = null, $api_version = 3) {
+    public function __construct($access_key, $secret_key, $api_host = null, $api_port = null, $api_version = 4) {
         $this->access_key = $access_key;
         $this->secret_key = $secret_key;
-        if ($api_host !== null)
+        if ($api_host !== null) {
             $this->api_host = $api_host;
-        if ($api_port !== null)
-            $this->api_port = $api_port;
-        if ($api_version === null) {
-            $this->api_version = 3;
-        } else {
-            $this->api_version = $api_version;
         }
+        if ($api_port !== null) {
+            $this->api_port = $api_port;
+        }
+        $this->api_version = $api_version;
     }
 
     public function get($resource_path, $params = array()) {
@@ -52,56 +50,65 @@ class Jscrambler {
 
     private function api_url() {
         $api_host_and_port = $this->api_host;
-        if ($this->api_port != 80)
+        if ($this->api_port != 80) {
             $api_host_and_port .= ":{$this->api_port}";
+        }
         return ($this->api_port == 443 ? "https" : "http") . "://" . $api_host_and_port . "/v{$this->api_version}";
     }
 
-    private function http_request($request_method, $resource_path, $params = null) {
-        $request_method = strtoupper($request_method);
-        $signed_data = null;
-        $url_query_part = null;
-        if ($request_method == 'POST')
+    private function http_request($request_method, $resource_path, $params = null) { 
+        $url = "{$this->api_url()}$resource_path";
+        if ($request_method == 'POST') {
             $signed_data = $this->signed_query($request_method, $resource_path, $params);
-        else
-            $url_query_part = '?' . $this->array_to_query($this->signed_query($request_method, $resource_path, $params));
-
-        $url = $this->api_url() . $resource_path . ($url_query_part ? $url_query_part : '');
-
-        $curl = curl_init($url);
-        if ($signed_data) {
-            if (!empty(CURLOPT_SAFE_UPLOAD)) {
+        } else {
+            $url .= "?{$this->array_to_query($this->signed_query($request_method, $resource_path, $params))}";
+        }
+        
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        if (isset($signed_data)) {
+            $json_data = self::json_stringify($signed_data);
+            $tmpfname = tempnam(sys_get_temp_dir(), 'php');
+            file_put_contents($tmpfname, $json_data);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, 
+                    array("@$tmpfname;type=application/json"));
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+            if (defined(CURLOPT_SAFE_UPLOAD)) {
                 curl_setopt($curl, CURLOPT_SAFE_UPLOAD, 0);
             }
-            curl_setopt($curl, CURLOPT_POST, 1);
-            @curl_setopt($curl, CURLOPT_POSTFIELDS, $signed_data);
         }
-        if (defined('CURLOPT_PROTOCOLS'))
-            curl_setopt($curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $request_method);
         curl_setopt($curl, CURLOPT_PORT, $this->api_port);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 0);
 
-
-        /* $transfer = array("transfer" => curl_exec($curl),
-          "info" => curl_getinfo($curl)); */
-
-        $transfer = curl_exec($curl);
+        $return = curl_exec($curl);
+        if (!$return) {
+            var_dump(curl_getinfo($curl));
+            var_dump(curl_errno($curl));
+            var_dump(curl_error($curl));
+        }
+        if (isset($signed_data)) {
+            unlink($tmpfname);
+        }
         curl_close($curl);
-        return $transfer;
+        return $return;
     }
 
-    private static function construct_file_params($js_files) {
-        if (!is_array($js_files))
-            $js_files = array($js_files);
-        $contents = array();
-        foreach ($js_files as $key => $file_path) {
-            if (!is_string($file_path) || ($data = @file_get_contents($file_path)) === false)
-                throw new Exception("Unable to read file '{$file_path}'");
-            $contents["file_$key"] = "@$file_path"; // curl_setopt: To post a file, prepend a filename with @
+    private static function build_file_b64_params(& $files) {
+        if (!is_array($files)) {
+            $files = array($files);
         }
-        return $contents;
+        foreach ($files as $index => $path) {
+            if (!is_string($path) || ($data = file_get_contents($path)) === false) {
+                throw new Exception("Unable to read file '$path'");
+            }
+            $files[$index] = array(
+                'name' => basename($path),
+                'b64' => base64_encode($data)
+            ); 
+        }
+        return $files;
     }
 
     private function signed_query($request_method, $resource_path, $params = array(), $timestamp = null) {
@@ -110,12 +117,9 @@ class Jscrambler {
     }
 
     private function signed_params($request_method, $resource_path, $params = array(), $timestamp = null) {
-        if ($request_method == 'POST' &&
-                array_key_exists('files', $params)) {
+        if ($request_method == 'POST' && array_key_exists('files', $params)) {
             try {
-                $file_params = self::construct_file_params($params['files']);
-                unset($params['files']);
-                $params = array_merge($params, $file_params);
+                self::build_file_b64_params($params['files']);
             } catch (Exception $e) {
                 echo $e->getFile() . "({$e->getLine()}): " . $e->getMessage() . "\nTrace:\n" . $e->getTraceAsString();
                 exit;
@@ -126,14 +130,10 @@ class Jscrambler {
         $auth_params['access_key'] = $this->access_key;
         $auth_params['user_agent'] = 'PHP';
         $auth_params['signature'] = $this->generate_hmac_signature($request_method, $resource_path, $auth_params);
-
         return $auth_params;
     }
-
+    
     private function generate_hmac_signature($request_method, $resource_path, $params = array()) {
-        foreach ($params as $key => $value)
-            if (strpos($key, "file_") !== FALSE)
-                $params[$key] = md5_file(substr($params[$key], 1));
         $hmac_signature_data = self::hmac_signature_data($request_method, $resource_path, $this->api_host, $params);
         $hashing_context = hash_init('sha256', HASH_HMAC, $this->secret_key);
         hash_update($hashing_context, $hmac_signature_data);
@@ -152,18 +152,20 @@ class Jscrambler {
     private static function array_to_query($array) {
         $kv = array();
         foreach ($array as $key => $value) {
+            if(is_array($value)) {
+                $value = self::json_stringify($value);
+            }
             $kv[] = self::urlencode($key) . '=' . self::urlencode($value);
         }
         return implode('&', $kv);
     }
 
-    private static function urlencode($data) {
-        $encoded_str = urlencode($data);
-        $encoded_str = str_replace("%7E", "~", $encoded_str);
-        $encoded_str = str_replace("+", "%20", $encoded_str);
-        return $encoded_str;
+    private static function urlencode($value) {
+        return str_replace("+", "%20", str_replace("%7E", "~", urlencode($value)));
     }
-
+    
+    private function json_stringify($value) {
+        return json_encode($value);
+    }
 }
-
 ?>
